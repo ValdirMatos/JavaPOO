94ca8bd9100f07de9568adbf6b0721f6f0b2ff4e
Valdir Matos <aquinobrasil@bol.com.br>
2023-06-25 10:30:17 -0300

    Subindo projeto-aula sem orientação a objetos - devSuperior

---
+++ src/Program.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        String nome1, nome2;
+        double valorPorHora1, valorPorHora2, total;
+        int horas1, horas2;
+        
+        System.out.println("Dados do primeiro funcionáro:");
+        nome1 = sc.next();
+        valorPorHora1 = sc.nextDouble();
+        horas1 = sc.nextInt();
+
+        System.out.println("Dados do segundo funcionáro:");
+        nome2 = sc.next();
+        valorPorHora2 = sc.nextDouble();
+        horas2 = sc.nextInt();
+
+        total = valorPorHora1 * horas1 + valorPorHora2 * horas2;
+
+        System.out.printf("Total = %.2f%n",  total);
+    }
+}
